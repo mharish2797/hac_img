1a3eb01bb82ec1099906ee9c40421d386795dc9d
Harish Mohan <mharish2797@gmail.com>
2020-05-25 19:46:20 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+        name='hac-img',
+		packages = ['hac-img'],
+        version='0.0.1',
+		license='MIT',
+        description='Hierarchical clustering algorithm for image clustering using Scikit-Image',
+		author = 'Harish Mohan',                   # Type in your name
+		author_email = 'mharish2797@gmail.com',      # Type in your E-Mail
+		url = 'https://mharish.dev',   # Provide either the link to your github or to your website
+		download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+		keywords = ['image', 'hierarchical', 'clustering'],   # Keywords that define your package best
+        py_modules=["hac_img"],
+        install_requires = ["scikit-image", "numpy", "Pillow"]
+        )
