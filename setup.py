3133bc8a10771adc84227b7e6474ea2355a09e25
Harish Mohan <mharish2797@gmail.com>
2020-05-25 19:54:35 -0700

    Fixing download_url

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ setuptools.setup(
 		author = 'Harish Mohan',                   # Type in your name
 		author_email = 'mharish2797@gmail.com',      # Type in your E-Mail
 		url = 'https://mharish.dev',   # Provide either the link to your github or to your website
-		download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+		download_url = 'https://github.com/mharish2797/hac_img/archive/0.0.1.tar.gz',    # I explain this later on
 		keywords = ['image', 'hierarchical', 'clustering'],   # Keywords that define your package best
         py_modules=["hac_img"],
         install_requires = ["scikit-image", "numpy", "Pillow"]
+		
         )
